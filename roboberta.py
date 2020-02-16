28be00923d0394b7a32a72ae37c1e6ab169c0796
alxfed <alxfed@gmail.com>
2020-02-16 17:34:55 -0600

    roberta

--- roboberta.py
+++ roboberta.py
@@ -21,6 +21,24 @@ def main():
     assert len(all_layers) == 25
     assert torch.all(all_layers[-1] == last_layer_features)
 
+    # Download RoBERTa already finetuned for MNLI
+    roberta = torch.hub.load('pytorch/fairseq', 'roberta.large.mnli')
+    roberta.eval()  # disable dropout for evaluation
+
+    with torch.no_grad():
+        # Encode a pair of sentences and make a prediction
+        tokens = roberta.encode('Roberta is a heavily optimized version of BERT.', 'Roberta is not very optimized.')
+        prediction = roberta.predict('mnli', tokens).argmax().item()
+        assert prediction == 0  # contradiction
+
+        # Encode another pair of sentences
+        tokens = roberta.encode('Roberta is a heavily optimized version of BERT.', 'Roberta is based on BERT.')
+        prediction = roberta.predict('mnli', tokens).argmax().item()
+        assert prediction == 2  # entailment
+
+    roberta.register_classification_head('new_task', num_classes=3)
+    logprobs = roberta.predict('new_task',
+                               tokens)  # tensor([[-1.1050, -1.0672, -1.1245]], grad_fn=<LogSoftmaxBackward>)
 
     return
 
