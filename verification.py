96b1857018e5f8f34b223e3f8a3969db0123c398
alxfed <alxfed@gmail.com>
2020-02-16 16:58:48 -0600

    verification

---
+++ verification.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""...
+"""
+from __future__ import print_function
+import torch
+
+
+def main():
+    x = torch.rand(5, 3)
+    print(x)
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
