2a8114ecb0ad8f323faef21ac241c946bcc6f98d
alxfed <alxfed@gmail.com>
2020-04-16 15:14:18 -0500

    postgre

--- post_alchemy.py
+++ post_alchemy.py
@@ -2,11 +2,15 @@
 """...
 """
 from sqlalchemy import *
-import psycopg2
+import psycopg2 as psycopg
 
 # Create a connection to a PostGreSQL database
 engine = create_engine('postgres://alxfed:postgres@localhost:5432/pg_db')
 
+def connect_pg():
+    return psycopg.connect(user='rick', host='localhost')
+
+enging = create_engine('postgres://', creator=connect_pg)
 
 
 def main():
